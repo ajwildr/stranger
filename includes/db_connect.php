96bfb7bd7eea2f7a1eb6dbfad69d2fad11087ecf
ajwildr <ajai515451@gmail.com>
2025-02-05 16:14:10 +0530

    database

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,11 +1,31 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "collaborative_work";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "collaborative_work";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+?>
+<?php
+function getDBConnection() {
+        $host = "stranger.mysql.database.azure.com";
+        $username = "zxwyielvwx";
+        $password = "IrnGhs5sDjaT4KT$";
+        $database = "collaborative_work";
+        
+        $conn = mysqli_connect($host, $username, $password, $database);
+        
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        
+        return $conn;
+    }
+    
+    // Create a global connection object
+    $conn = getDBConnection();
+    
 ?>
